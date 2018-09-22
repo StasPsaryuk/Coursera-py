11579cb7de36838f46030ede0da5d79c236db421
StasPsaryuk <staspsaryuk@gmail.com>
2018-09-22 14:48:11 +0300

    Task2 - comleted

--- mail-py/Task2-Stairs.py
+++ mail-py/Task2-Stairs.py
@@ -1,1 +1,16 @@
-#test
+import sys
+num_steps = int(sys.argv[1])
+
+
+for i in range(1, num_steps+1):
+    print(" "*(num_steps-i), "#"*i, sep="")
+
+""" 
+Result for python Task2-Stairs.py 5
+
+   #
+  ##
+ ###
+####
+
+"""
