f6c353f314923a332c5de807b246de6a7831a092
StasPsaryuk <staspsaryuk@gmail.com>
2018-09-25 21:27:03 +0300

    max_element

---
+++ mail-py/count_max_element.py
@@ -0,0 +1,15 @@
+maximum = 0
+num_maximal = 0
+element = -1
+while element != 0:
+    element = int(input())
+    if element > maximum:
+        maximum, num_maximal = element, 1
+    elif element == maximum:
+        num_maximal += 1
+print(num_maximal)
+
+
+#Условие
+#Последовательность состоит из натуральных чисел и завершается числом 0.
+#Определите, сколько элементов этой последовательности равны ее наибольшему элементу.
